b422dc8c167164c676ee8affed8658b5871d4a2d
john2796 <benedictmiranda27@yahoo.com>
2018-12-03 12:18:53 -0800

    still working

--- js/index.js
+++ js/index.js
@@ -39,24 +39,41 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+const container = document.querySelector('.container');
 const logo = document.getElementById("logo-img");
 const ctaImg = document.querySelector('#cta-img');
-const middleImg = document.querySelectorAll('#middle-img');
-let anchor = document.querySelectorAll('a');
+const middleImg = document.querySelector('#middle-img');
+
+const nav = document.querySelector('nav');
 
 
+// images
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 ctaImg.setAttribute('src', siteContent['cta']['img-src'])
 middleImg.setAttribute('src', siteContent['main-content']['img-src'])
-// anchor[0].textContent = "testing"
-anchor = Array.from(anchor);
 
-const nav = siteContent.nav
-for (let key in nav) {
-  anchor.forEach(a => {
-    a.appendChild(nav[key]);
-  })
+// nav
+const service = document.createElement('a');
+const product = document.createElement('a');
+const vision = document.createElement('a');
+const features = document.createElement('a');
+const about = document.createElement('a');
+const contact = document.createElement('a');
+service.textContent = siteContent['nav']["nav-item-1"]
+product.textContent = siteContent['nav']["nav-item-2"]
+vision.textContent = siteContent['nav']["nav-item-3"]
+features.textContent = siteContent['nav']["nav-item-4"]
+about.textContent = siteContent['nav']["nav-item-5"]
+contact.textContent = siteContent['nav']["nav-item-6"]
 
-}
+nav.prepend(service, product, vision, features, about, contact);
+// const navItems = siteContent['nav'];
+// for (let key in navItems) {
+//   const element = navItems[key];
+//   let anchor = document.createElement('a');
+//   anchor.textContent = element;
+//   nav.append(anchor);
+// }
+
 
 
